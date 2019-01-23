1a22d5c9385de7596bf5399c9d26d09ec611ac50
Hans <hans@wisy.app>
2019-01-23 17:32:32 -0500

    footer now is using social media

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,28 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Content } from "../layout";
-import instaIcon from "../../assets/instagram.svg";
-import { footerContent, footerContainer, ul, li } from "./style";
+import SocialMedia from "../SocialMedia";
+import { footerContent, footerContainer } from "./style";
 
 function Footer({ maxWidth }) {
   return (
     <footer css={footerContainer}>
       <Content maxWidth={maxWidth}>
         <div css={footerContent}>
-          <div>
-            Social media
-            <ul css={ul}>
-              <li css={li}>
-                <a
-                  href="https://www.instagram.com/hans.lebon/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={instaIcon} alt="Instagram page" />
-                </a>
-              </li>
-            </ul>
-          </div>
+          <SocialMedia />
           <div>
             Hans
             <span style={{ color: "rgba(33,150,243,1)", margin: "0 0.4rem" }}>
