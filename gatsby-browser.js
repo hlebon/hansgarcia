23209a7f9cc59251b46c3578694f174d60481031
Hans <hans@wisy.app>
2019-01-23 23:00:38 -0500

    function to reload sw

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,12 @@
 require("./src/styles/prims.css");
+
+exports.onServiceWorkerUpdateFound = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
