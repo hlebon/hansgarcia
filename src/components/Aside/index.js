27f658d225480a9a40183b1b54800e5d392b997d
Hans <hans@wisy.app>
2019-01-23 21:59:32 -0500

    style: const style created to separete style from component

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -4,15 +4,15 @@ import Bio from "../Bio";
 import Signup from "../SignUp";
 import SocialMedia from "../SocialMedia";
 
+const asideStyle = css`
+  padding: 1em;
+  border-left: 1px solid #b0def3;
+  background-color: white;
+`;
+
 function Aside() {
   return (
-    <aside
-      css={css`
-        padding: 1em;
-        border-left: 1px solid #b0def3;
-        background-color: white;
-      `}
-    >
+    <aside css={asideStyle}>
       <Bio />
       <Signup clean />
       <SocialMedia title="Follow me on" />
