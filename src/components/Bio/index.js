9a3adfb313753466ff9243bd7293e4c8c707c9d1
Hans <hans@wisy.app>
2019-01-23 21:54:22 -0500

    refactor: removing header tag

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -5,16 +5,14 @@ function Bio() {
   return (
     <section key="about">
       <article>
-        <header>
-          <h3
-            style={{
-              margin: `${rhythm(2 / 4)} 0 ${rhythm(2 / 4)} 0`
-            }}
-          >
-            About me
-          </h3>
-          <p>Hello, my name is Hans, I am a software engineer from Panama.</p>
-        </header>
+        <h3
+          style={{
+            margin: `${rhythm(2 / 4)} 0 ${rhythm(2 / 4)} 0`
+          }}
+        >
+          About me
+        </h3>
+        <p>Hello, my name is Hans, I am a software engineer from Panama.</p>
       </article>
     </section>
   );
