4470fba3ab4954c0485b02eb99a0483346de9e3d
Hans <hans@wisy.app>
2019-01-26 21:17:15 -0500

    style: header padding

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -4,7 +4,7 @@ import BgHero from "../../assets/images/panama.jpg";
 
 export const heroStyle = css`
   width: 100%;
-  padding: 5rem 1rem 4rem 1rem;
+  padding: 10rem 1rem 6rem 1rem;
   margin-bottom: 3rem;
   justify-content: center;
   overflow: hidden;
