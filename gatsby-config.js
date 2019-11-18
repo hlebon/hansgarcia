8c09ae0a58a730ff9dea190e51a63854ed71c1a1
hlebon <hanslgarcia@gmail.com>
2019-11-18 12:08:40 -0600

    config mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,56 +3,64 @@ module.exports = {
     title: `Hans García`,
     subtitle: `Full Stack developer, pizza and coffee lover`,
     techstack: [
-      "javascript",
-      "node",
-      "react",
-      "react-native",
-      "graphql",
-      "firebase",
-      "posgresql",
-      ".net"
+      'javascript',
+      'node',
+      'react',
+      'react-native',
+      'graphql',
+      'firebase',
+      'posgresql',
+      '.net',
     ],
     description: `Web development articles, javascript, react, react-native, firestore, aws, PWA and more.`,
     author: {
-      name: "Hans García",
+      name: 'Hans García',
       minibio: `
       <strong>hanslebon.com</strong> is a personal blog about software development.
-    `
+    `,
     },
     siteUrl: `https://hanslebon.com/`,
-    keywords: ["Video Blogger", "Javascript", "React", "PWA"],
-    canonicalUrl: "https://hanslebon.com",
-    image: "",
+    keywords: ['Video Blogger', 'Javascript', 'React', 'PWA'],
+    canonicalUrl: 'https://hanslebon.com',
+    image: '',
     organization: {
-      name: "Hans García",
-      url: "https://hanslebon.com",
-      logo: ""
+      name: 'Hans García',
+      url: 'https://hanslebon.com',
+      logo: '',
     },
     social: {
-      twitter: "@HansLGarcia",
-      fbAppID: ""
-    }
+      twitter: '@HansLGarcia',
+      fbAppID: '',
+    },
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-emotion",
+      resolve: 'gatsby-plugin-emotion',
       options: {
         // Accepts all options defined by `babel-plugin-emotion` plugin.
         sourceMap: true,
-        autoLabel: process.env.NODE_ENV !== "production",
-        labelFormat: "[local]",
-        cssPropOptimization: true
-      }
+        autoLabel: process.env.NODE_ENV !== 'production',
+        labelFormat: '[local]',
+        cssPropOptimization: true,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/`,
-        name: `post`
-      }
+        name: `post`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
     },
     // for gatsby image
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -63,42 +71,42 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
-              showCaptions: true
-            }
+              showCaptions: true,
+            },
           },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
               noInlineHighlight: false,
               languageExtensions: [
                 {
-                  language: "superscript",
-                  extend: "javascript",
+                  language: 'superscript',
+                  extend: 'javascript',
                   definition: {
-                    superscript_types: /(SuperType)/
+                    superscript_types: /(SuperType)/,
                   },
                   insertBefore: {
                     function: {
-                      superscript_keywords: /(superif|superelse)/
-                    }
-                  }
-                }
-              ]
-            }
-          }
-        ]
-      }
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-130239906-1",
+        trackingId: 'UA-130239906-1',
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
@@ -106,13 +114,13 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
         // Enables Google Optimize using your container Id
-        optimizeId: "GTM-5344V8G",
+        optimizeId: 'GTM-5344V8G',
         // Any additional create only fields (optional)
         sampleRate: 5,
-        siteSpeedSampleRate: 10
-      }
+        siteSpeedSampleRate: 10,
+      },
     },
     {
       resolve: `gatsby-plugin-feed`,
@@ -133,26 +141,30 @@ module.exports = {
           {
             serialize: ({ query: { site, allMarkdownRemark } }) =>
               allMarkdownRemark.edges.map(edge => {
-                const { siteUrl } = site.siteMetadata;
+                const { siteUrl } = site.siteMetadata
                 const postText = `
                 <div style="margin-top=55px; font-style: italic;">(Este articulo fue publicado en mi blog hanslebon.com. Puedes leer online <a href="${siteUrl +
                   edge.node.fields.slug}">clicking here</a>.)</div>
-              `;
+              `
 
-                let { html } = edge.node;
+                let { html } = edge.node
                 html = html
                   .replace(/href="\//g, `href="${siteUrl}/`)
                   .replace(/src="\//g, `src="${siteUrl}/`)
                   .replace(/"\/static\//g, `"${siteUrl}/static/`)
-                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`);
+                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
 
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": html + postText }]
-                });
+                  custom_elements: [
+                    {
+                      'content:encoded': html + postText,
+                    },
+                  ],
+                })
               }),
             query: `
               {
@@ -177,11 +189,11 @@ module.exports = {
               }
             }
             `,
-            output: "/rss.xml",
-            title: "Hans's Blog RSS Feed"
-          }
-        ]
-      }
+            output: '/rss.xml',
+            title: "Hans's Blog RSS Feed",
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -194,9 +206,9 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/assets/images/logo.PNG` // This path is relative to the root of the site.
-      }
+        icon: `src/assets/images/logo.PNG`, // This path is relative to the root of the site.
+      },
     },
-    "gatsby-plugin-offline"
-  ]
-};
+    'gatsby-plugin-offline',
+  ],
+}
