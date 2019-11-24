421e58ad7c001d7b8d2dc185069b834cb280942f
Hans Garcia <hansgarcia@Hanss-MacBook-Pro.local>
2019-11-24 16:23:26 -0500

    update

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,9 @@
 export const getDate = (date, format = {}) => {
-  const { y = "numeric", m = "long", d = "numeric" } = format;
-  const newDate = new Date(date).toLocaleDateString("en-US", {
+  const { y = 'numeric', m = 'long', d = 'numeric' } = format;
+  const newDate = new Date(date).toLocaleDateString('en-US', {
     year: y,
     month: m,
-    day: d
+    day: d,
   });
   return newDate;
 };
